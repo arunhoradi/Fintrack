a9ee0989a7080288aba0a140bb78f0663058f972
Arun Horadi <arunhoradi@gmail.com.com>
2025-03-03 11:25:08 +0000

    sign-in/sign-up files created

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,17 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
+    <div className="fixed top-0">
+        <nav>
+          <Link href="/">
+          <Image src={"/fintrack-logo.png"} alt="Fintrack app" width={200} height={60} className="h-12 w-auto object-contain"/>
+
+          </Link>
+        </nav>
       <SignedOut>
         <SignInButton />
         <SignUpButton />
--- middleware.js
+++ middleware.js
@@ -1,12 +1,25 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transactions(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (userId && isProtectedRoute(req)) {
+    const { reDirectToSignIn } = await auth();
+    return reDirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
